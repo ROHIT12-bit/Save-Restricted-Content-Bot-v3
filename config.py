fc396a70426c11f38ea0716545ce734b8ad04eb9
ROHIT12-bit <reddyrohit823@gmail.com>
2025-12-16 19:19:01 +0530

    ifk

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DB_NAME      = os.getenv("DB_NAME", "telegram_downloader")
 OWNER_ID     = list(map(int, os.getenv("OWNER_ID", "8367080346").split()))  # space-separated list
 STRING       = os.getenv("STRING", None)  # optional session string
 LOG_GROUP    = int(os.getenv("LOG_GROUP", "-1003350185458"))
-FORCE_SUB    = int(os.getenv("FORCE_SUB", ""))
+FORCE_SUB    = int(os.getenv("FORCE_SUB", "-1002747099183"))
 
 # ─── SECURITY KEYS ──────────────────────────────────────────────────────────────
 MASTER_KEY   = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq")  # session encryption
@@ -76,4 +76,5 @@ P0 = {
 # ════════════════════════════════════════════════════════════════════════════════
 # ░ DEVGAGAN
 # ════════════════════════════════════════════════════════════════════════════════
+
 
