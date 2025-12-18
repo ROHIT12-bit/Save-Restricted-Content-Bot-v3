b7c627bd4fec0e7b35d6d595ed721f7309f2dc1c
ROHIT12-bit <reddyrohit823@gmail.com>
2025-12-18 06:27:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 devgagan : https://github.com/devgaganin.  
+# Copyright (c) 2025 RioShin : https://github.com/Rioshin2025.  
 # Licensed under the GNU General Public License v3.0.  
 # See LICENSE file in the repository root for full license text.
 
@@ -9,10 +9,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    # Render the welcome page with animated "Team SPY" text
+    # Render the welcome page with animated "Botskingdoms" text
     return render_template("welcome.html")
 
 if __name__ == "__main__":
     # Default to port 5000 if PORT is not set in the environment
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
