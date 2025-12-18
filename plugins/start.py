4dc59c315dd36a0c54053b58e99c06252d412a7f
ROHIT12-bit <reddyrohit823@gmail.com>
2025-12-18 08:33:05 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -152,84 +152,29 @@ async def on_help_navigation(client, callback_query):
     await send_or_edit_help_page(client, callback_query.message, page_number)
     await callback_query.answer()
 
-# ----------------- Terms and Plan commands -----------------
-@app.on_message(filters.command("terms") & filters.private)
-async def terms(client, message):
-    terms_text = (
-        "> 📜 **Terms and Conditions** 📜\n\n"
-        "✨ We are not responsible for user deeds, and we do not promote copyrighted content. If any user engages in such activities, it is solely their responsibility.\n"
-        "✨ Upon purchase, we do not guarantee the uptime, downtime, or the validity of the plan. __Authorization and banning of users are at our discretion; we reserve the right to ban or authorize users at any time.__\n"
-        "✨ Payment to us **__does not guarantee__** authorization for the /batch command. All decisions regarding authorization are made at our discretion and mood.\n"
-    )
-     
-    buttons = InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/kingofpatal")],
-        ]
-    )
-    await message.reply_text(terms_text, reply_markup=buttons)
-
-@app.on_message(filters.command("plan") & filters.private)
+# -----------------  Plan commands -----------------
+@app.on_message(filters.command("plans") & filters.private)
 async def plan(client, message):
-    plan_text = (
-        "> 💰 **Premium Price**:\n\n Starting from $2 or 200 INR accepted via **__Amazon Gift Card__** (terms and conditions apply).\n"
-        "📥 **Download Limit**: Users can download up to 100,000 files in a single batch command.\n"
-        "🛑 **Batch**: You will get two modes /bulk and /batch.\n"
-        "   - Users are advised to wait for the process to automatically cancel before proceeding with any downloads or uploads.\n\n"
-        "📜 **Terms and Conditions**: For further details and complete terms and conditions, please send /terms.\n"
-    )
+    plan_text = """ 💖 Premium Benefits
+~ High-speed uploads.
+~ Fetch content from private/private channel.
+~ Fetch content from private/public group.
+~ Join any chat by sending invite link.
+~ Supports Batch system for multiple files
+
+💵 Plans & Prices (No Bargains)
+~ 7 Days (1$/₹50)
+~ 30 Days (2$/₹100)
+~ Quaterly (3$/₹150)  
+~ Half-Year (4$/₹300)  
+~ Full-Year (5$/₹600)  
+
+✅ Click the button below for your subscription!"""
      
     buttons = InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton("📜 See Terms", callback_data="see_terms")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/kingofpatal")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/RioShin")],
         ]
     )
     await message.reply_text(plan_text, reply_markup=buttons)
-
-@app.on_callback_query(filters.regex("see_plan"))
-async def see_plan(client, callback_query):
-    plan_text = (
-        "> 💰**Premium Price**\n\n Starting from $2 or 200 INR accepted via **__Amazon Gift Card__** (terms and conditions apply).\n"
-        "📥 **Download Limit**: Users can download up to 100,000 files in a single batch command.\n"
-        "🛑 **Batch**: You will get two modes /bulk and /batch.\n"
-        "   - Users are advised to wait for the process to automatically cancel before proceeding with any downloads or uploads.\n\n"
-        "📜 **Terms and Conditions**: For further details and complete terms and conditions, please send /terms or click See Terms👇\n"
-    )
-     
-    buttons = InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton("📜 See Terms", callback_data="see_terms")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/kingofpatal")],
-        ]
-    )
-    await callback_query.message.edit_text(plan_text, reply_markup=buttons)
-
-@app.on_callback_query(filters.regex("see_terms"))
-async def see_terms(client, callback_query):
-    terms_text = (
-        "> 📜 **Terms and Conditions** 📜\n\n"
-        "✨ We are not responsible for user deeds, and we do not promote copyrighted content. If any user engages in such activities, it is solely their responsibility.\n"
-        "✨ Upon purchase, we do not guarantee the uptime, downtime, or the validity of the plan. __Authorization and banning of users are at our discretion; we reserve the right to ban or authorize users at any time.__\n"
-        "✨ Payment to us **__does not guarantee__** authorization for the /batch command. All decisions regarding authorization are made at our discretion and mood.\n"
-    )
-     
-    buttons = InlineKeyboardMarkup(
-        [
-            [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/kingofpatal")],
-        ]
-    )
-    await callback_query.message.edit_text(terms_text, reply_markup=buttons)
-
-
-
-
-
-
-
-
-
-
 
